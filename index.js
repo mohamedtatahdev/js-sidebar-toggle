43e3070df6a3e7d25c09f22a075301122b9c7d66
mohamedtatahdev <mohamed.tatah69@gmail.com>
2024-04-18 20:14:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector('.content');
+
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
